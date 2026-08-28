caa8384e5111f393f8270742938c88278487fd8a
jude-z <ccccssung@gmail.com>
2026-08-28 22:23:12 +0900

    ## Redis CacheManger 설정 및 테스트 - RedisCacheManger, RedisCacheConfiguration 설정(CacheConfig.java) - 채팅방(chatRoom), 채팅참여자(chatParticipant), 채팅메세지(chatMessage) 캐싱 - CacheServiceTest(ChatRepository mocking이용)

---
+++ chat-server/src/main/java/jude/carrot/chatserver/service/ChatCacheService.java
@@ -0,0 +1,35 @@
+package jude.carrot.chatserver.service;
+
+import jude.carrot.chatserver.key.redis.ChatKeyGenerator;
+import jude.carrot.infra.entity.chat.ChatMessage;
+import jude.carrot.infra.entity.chat.ChatParticipant;
+import jude.carrot.infra.entity.chat.ChatRoom;
+import jude.carrot.infra.repository.chat.ChatRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class ChatCacheService {
+
+    private final ChatRepository chatRepository;
+
+    @Cacheable(cacheNames = "chatRoom", key = "#chatRoomId", unless = "#result.isEmpty()")
+    public Optional<ChatRoom> fetchChatRoom(Long chatRoomId){
+        return chatRepository.fetchChatRoom(chatRoomId);
+    }
+
+    @Cacheable(cacheNames = "chatParticipant", key = "#chatRoomId + '::' + #userId", unless = "#result.isEmpty()")
+    public Optional<ChatParticipant> fetchChatParticipant(Long chatRoomId, Long userId){
+        return chatRepository.fetchChatParticipant(chatRoomId, userId);
+    }
+
+    @Cacheable(cacheNames = "chatMessage", key = "#chatMessageKey", unless = "#result.isEmpty()")
+    public Optional<ChatMessage> fetchChatMessage(String chatMessageKey){
+        String chatMessageId = ChatKeyGenerator.parseChatMessageId(chatMessageKey);
+        return chatRepository.fetchChatMessage(chatMessageId);
+    }
+}
