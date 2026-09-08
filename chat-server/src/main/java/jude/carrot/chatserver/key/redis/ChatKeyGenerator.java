b1eaa56b33affcaea8961948ea87e38c8a833f25
jude-z <ccccssung@gmail.com>
2026-09-08 22:42:03 +0900

    ## 리뷰 수정 - final 선언 - parseChatMessageId 로직 추가

--- chat-server/src/main/java/jude/carrot/chatserver/key/redis/ChatKeyGenerator.java
+++ chat-server/src/main/java/jude/carrot/chatserver/key/redis/ChatKeyGenerator.java
@@ -1,9 +1,10 @@
 package jude.carrot.chatserver.key.redis;
 
 public class ChatKeyGenerator {
-    private static String chatMessageFormat = "chatMessage::%s";
-    private static String chatRoomMessageFormat = "chatRoom::%s";
-    private static String readStatusFormat = "chatParticipantId::%s::%s";
+    private static final String chatMessageFormat = "chatMessage::%s";
+    private static final String chatRoomMessageFormat = "chatRoom::%s";
+    private static final String readStatusFormat = "chatParticipantId::%s::%s";
+    private static final String token = "::";
 
 
     public static String generateChatMessageKey(String snowflakeId){
@@ -11,6 +12,9 @@ public class ChatKeyGenerator {
     }
 
     public static String parseChatMessageId(String chatMessageKey){
+        String[] split = chatMessageKey.split(token);
+        if(split.length != 2) return null;
+        if(!split[0].equals("chatMessage")) return null;
         return chatMessageKey.split("::")[1];
     }
 
